3e71efe807b51724cf76fded09f07db7e024ac61
bjick84 <bjick@web.de>
2023-01-18 10:57:35 +0100

    3. Fkt. Grundrechenarten :

--- script_01.js
+++ script_01.js
@@ -11,8 +11,37 @@
 4. Ausgabe in Konsole : check!
 */
 
+// module: addition a + b |  test:
+output(add(2, 3));
+output(add(2.5, -3));
+output(add(2, 0));
+function add(a, b) {
+	return a + b;
+}
 
+// module: subtraktion a - b |  test:
+output(sub(7, 6));
+output(sub(9.5, -15));
+output(sub(8, 0));
+function sub(a, b) {
+	return a - b;
+}
 
+// module: multiplikation a * b |  test:
+output(multi(7, 6));
+output(multi(9.5, -15));
+output(multi(8, 0));
+function multi(a, b) {
+	return a * b;
+}
+
+// module: division a / b |  test:
+output(div(7, 6));
+output(div(9.5, -15));
+output(div(8, 0));
+function div(a, b) {
+	return a / b;
+}
 
 // module:  output | test:
 // output("hello");
